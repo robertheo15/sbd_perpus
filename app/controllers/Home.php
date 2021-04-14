ba859a15d2d4d297d029dc31fd439c9ce5d31bcd
kennethliem <kennethliem991@gmail.com>
2021-04-14 08:07:55 +0700

    Update Home.php
    Pidahin Search ke bawah sesuai mockup

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -7,9 +7,9 @@ class Home extends Controller
         $data['judul'] = 'Beranda';
         // $data['nama'] = $this->model('User_model')->getUser();
         $this->view('templates/header', $data);
-        $this->view('home/search', $data);
         $this->view('home/carousell', $data);
         $this->view('home/welcome', $data);
+        $this->view('home/search', $data);
         $this->view('home/index', $data);
         $this->view('home/social', $data);
         $this->view('templates/footer');
