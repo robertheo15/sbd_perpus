4382d5709f12f78beae62a86a584d584b47a6a35
robertheo <robertheo.rt@gmail.com>
2021-05-19 20:47:44 +0700

    update

--- app/views/admin/book/form-control-edit.php
+++ app/views/admin/book/form-control-edit.php
@@ -1,20 +1,37 @@
+<input type="hidden" name='idBook' value="<?= $data['books']['id_book']; ?>">
 <div class="mb-3">
-  <label for="info" class="form-label">Info</label>
-  <input type="text" class="form-control" id="info" name="info" required>
+  <label for="formFile" class="form-label">Foto cover</label>
+  <input class="form-control" type="file" id="formFile" name="formFile">
 </div>
 
 <div class="mb-3">
-  <label for="address" class="form-label">Alamat</label>
-  <input type="text" class="form-control" id="address" name="address">
+  <label for="titleBook" class="form-label">Judul buku</label>
+  <input type="text" class="form-control" id="titleBook" name="titleBook" required value="<?= $data['books']['title_book']; ?>">
 </div>
 
 <div class="mb-3">
-  <label for="email" class="form-label">Email</label>
-  <input type="text" class="form-control" id="email" name="email">
+  <label for="authorName" class="form-label">Penulis</label>
+  <input type="text" class="form-control" id="authorName" name="authorName" value="<?= $data['books']['author_name']; ?>">
 </div>
 
 <div class="mb-3">
-  <label for="phone" class="form-label">No Hp</label>
-  <input type="text" class="form-control" id="phone" name="phone">
+  <label for="yearBook" class="form-label">Tahun terbit</label>
+  <input type="date" class="form-control" id="yearBook" name="yearBook" value="<?= $data['books']['year_book']; ?>">
 </div>
-<button type="submit" class="btn btn-primary">Edit</button>
+
+<div class="mb-3">
+  <label for="quantity" class="form-label">Jumlah</label>
+  <input type="text" class="form-control" id="quantity" name="quantity" value="<?= $data['books']['quantity']; ?>">
+</div>
+
+<div class="mb-3">
+  <label for="bookType" class="form-label">Tipe buku</label>
+  <input type="text" class="form-control" id="bookType" name="bookType" value="<?= $data['books']['book_type']; ?>">
+</div>
+
+<div class="mb-3">
+  <label for="bookPrice" class="form-label">Harga</label>
+  <input type="text" class="form-control" id="bookPrice" name="bookPrice" value="<?= $data['books']['book_price']; ?>">
+</div>
+
+<button type="submit" class="btn btn-primary">Post</button>
